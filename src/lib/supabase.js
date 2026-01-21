b8cb44d18888a1f7320f1f7daadccb3cb106b69b
Praveen9879 <praveenpatel3551@gmail.com>
2026-01-21 23:05:51 +0530

    fix: Enforce PKCE auth flow security

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -7,4 +7,11 @@ if (!supabaseUrl || !supabaseAnonKey) {
     console.error('Supabase URL or Key env variables are missing!')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        flowType: 'pkce',
+        detectSessionInUrl: true,
+        autoRefreshToken: true,
+        persistSession: true
+    }
+})
